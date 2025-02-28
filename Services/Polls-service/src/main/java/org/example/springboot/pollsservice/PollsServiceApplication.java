ab68ba46c1a90abfd26900dffddebe25661af40e
portner-boop <damirmagdeev1054@gmail.com>
2025-02-28 23:20:24 +0400

    Config server, Eureka server, postgres docker

---
+++ Services/Polls-service/src/main/java/org/example/springboot/pollsservice/PollsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot.pollsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollsServiceApplication.class, args);
+    }
+
+}
